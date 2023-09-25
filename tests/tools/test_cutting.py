5407842efa4396d52bcb7e873f26497c932f17da
Philipp Seitz <philipp.seitz@tum.de>
2023-09-25 16:41:35 +0200

    Fix cutting test

--- tests/tools/test_cutting.py
+++ tests/tools/test_cutting.py
@@ -6,7 +6,7 @@ from src.tools import cut_circuit
 def test_cut_circuit() -> None:
     """_summary_"""
     circuit = create_quantum_only_ghz(5)
-    experiments = cut_circuit(circuit, [2, 3])
+    experiments, _ = cut_circuit(circuit, [2, 3])
     assert len(experiments) == 2
     assert experiments[0].circuits[0].num_qubits == 2
     assert experiments[1].circuits[1].num_qubits == 3
