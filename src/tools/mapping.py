7b3b078509aec603fe350f93ba51ca29c4dc7879
Philipp Seitz <philipp.seitz@tum.de>
2023-09-26 11:15:45 +0200

    Add mapping as pass in online optimization

--- src/tools/mapping.py
+++ src/tools/mapping.py
@@ -1,4 +1,6 @@
 """Mapping to hardware using mqt QMAP."""
+from typing import Tuple
+
 from qiskit import QuantumCircuit
 from qiskit.transpiler import PassManager
 from qiskit.transpiler.passes import SabreLayout, SabreSwap
@@ -6,16 +8,19 @@ from qiskit.transpiler.passes import SabreLayout, SabreSwap
 from src.common import IBMQBackend
 
 
-def map_circuit(circuit: QuantumCircuit, backend: IBMQBackend) -> QuantumCircuit:
+def map_circuit(
+    circuit: QuantumCircuit, backend: IBMQBackend
+) -> Tuple[QuantumCircuit, PassManager]:
     """_summary_
 
     Args:
         circuit (QuantumCircuit): _description_
+        backend (IBMQBackend): _description_
 
     Returns:
-        QuantumCircuit: _description_
+        Tuple[QuantumCircuit, PassManager]: _description_
     """
     target = backend.value().target
     mapping_and_routing = PassManager([SabreLayout(target), SabreSwap(target)])
     circuit = mapping_and_routing.run(circuit)
-    return circuit
+    return circuit, mapping_and_routing
--- src/tools/optimizing.py
+++ src/tools/optimizing.py
@@ -4,6 +4,7 @@ from qiskit import QuantumCircuit
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 
 from src.common import IBMQBackend
+from .mapping import map_circuit
 
 
 def optimize_circuit_offline(
@@ -25,10 +26,11 @@ def optimize_circuit_offline(
     pass_manager = generate_preset_pass_manager(
         3, backend.value()
     )  # TODO eventually remove dependency
-    pass_manager.routing = None
-    pass_manager.translation = None
+    pass_manager.layout = None
     pass_manager.optimization = None
+    pass_manager.routing = None
     pass_manager.scheduling = None
+    pass_manager.translation = None
     return pass_manager.run(circuit)
 
 
@@ -49,5 +51,5 @@ def optimize_circuit_online(
     """
     pass_manager = generate_preset_pass_manager(3, backend.value())
     pass_manager.init = None
-    pass_manager.layout = None
+    _, pass_manager.layout = map_circuit(circuit, backend)
     return pass_manager.run(circuit)
