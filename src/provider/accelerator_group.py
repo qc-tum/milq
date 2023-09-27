10d906b30ba58cb5861ddd87b29c51c56a52cba8
Philipp Seitz <philipp.seitz@tum.de>
2023-09-27 16:15:15 +0200

    Build first example of experiment parallelism

--- src/provider/accelerator_group.py
+++ src/provider/accelerator_group.py
@@ -1,8 +1,10 @@
 """"""
+from multiprocessing import Pool, current_process
 from typing import Dict, List
 
 from qiskit import QuantumCircuit
 
+from src.common import Experiment
 from .accelerator import Accelerator
 
 
@@ -39,3 +41,32 @@ class AcceleratorGroup:
             counts.append(accelerator.run_and_get_counts(circuit))
         # TODO do some magic to figure out which counts belong to which circuit
         return counts
+
+    def run_experiments(self, experiments: List[Experiment]) -> List[Experiment]:
+        """_summary_
+
+        Args:
+            experiments (List[Experiment]): _description_
+
+        Returns:
+           List[Experiment]: _description_
+        """
+        with Pool(processes=len(self.accelerators)) as pool:
+            results = []
+            for experiment in experiments:
+                result = pool.apply_async(_run_func, [self.accelerators, experiment])
+                results.append(result)
+            results = [result.get() for result in results]
+
+        return results
+
+
+def _run_func(accs: List[Accelerator], exp: Experiment) -> Experiment:
+    pool_id = current_process()._identity[0] - 1  # TODO fix somehow
+    try:
+        exp.result_counts = [
+            accs[pool_id].run_and_get_counts(circ) for circ in exp.circuits
+        ]
+    except Exception as e:
+        print(e)
+    return exp
