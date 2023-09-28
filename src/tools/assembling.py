81ac40dae19a920229a89fb6cecc26cceb3ca19b
Philipp Seitz <philipp.seitz@tum.de>
2023-09-28 13:05:07 +0200

    Keep original registers in cirucit assembly

--- src/tools/assembling.py
+++ src/tools/assembling.py
@@ -1,5 +1,5 @@
 """Assemble a single circuit from multiple independent ones."""
-from qiskit import QuantumCircuit
+from qiskit import ClassicalRegister, QuantumCircuit, QuantumRegister
 from qiskit.quantum_info import PauliList
 
 from src.common import CircuitJob, CombinedJob
@@ -14,13 +14,17 @@ def assemble_circuit(circuits: list[QuantumCircuit]) -> QuantumCircuit:
     Returns:
         QuantumCircuit: _description_
     """
-    qubits, clbits = [
-        sum(x)
-        for x in zip(
-            *[(circuit.num_qubits, circuit.num_clbits) for circuit in circuits]
-        )
-    ]
-    composed_circuit = QuantumCircuit(qubits, clbits)
+    composed_circuit = QuantumCircuit()
+    for idx, circuit in enumerate(circuits):
+        for creg in circuit.cregs:
+            composed_circuit.add_register(
+                ClassicalRegister(creg.size, f"{idx}_{creg.name}")
+            )
+        for qreg in circuit.qregs:
+            composed_circuit.add_register(
+                QuantumRegister(qreg.size, f"{idx}_{qreg.name}")
+            )
+
     qubits, clbits = 0, 0
     for circuit in circuits:
         composed_circuit.compose(
@@ -57,6 +61,7 @@ def assemble_job(circuit_jobs: list[CircuitJob]) -> CombinedJob:
         qubit_count += job.instance.num_qubits
         observable = observable.expand(job.observable)
         combined_job.partition_lables.append(job.partition_lable)
+        combined_job.uuids.append(job.uuid)
     combined_job.instance = assemble_circuit(circuits)
     combined_job.observable = observable
     return combined_job
