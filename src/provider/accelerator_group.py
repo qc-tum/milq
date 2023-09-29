9d961a83348e0265d19ba344ff64c4320af1a206
Philipp Seitz <philipp.seitz@tum.de>
2023-09-29 15:40:07 +0200

    Build run jobs functionality

--- src/provider/accelerator_group.py
+++ src/provider/accelerator_group.py
@@ -1,9 +1,10 @@
 """"""
+from itertools import zip_longest
 from multiprocessing import Pool, current_process
 
 from qiskit import QuantumCircuit
 
-from src.common import Experiment
+from src.common import CombinedJob, Experiment, ScheduledJob
 from .accelerator import Accelerator
 
 
@@ -50,6 +51,28 @@ class AcceleratorGroup:
         # TODO do some magic to figure out which counts belong to which circuit
         return counts
 
+    def run_jobs(self, jobs: list[ScheduledJob]) -> list[CombinedJob]:
+        """_summary_
+
+        Args:
+            jobs (list[ScheduledJob]): _description_
+
+        Returns:
+            list[ScheduledJob]: _description_
+        """
+        jobs_per_qpu = {
+            qpu: [job for job in jobs if job.qpu == qpu]
+            for qpu, _ in enumerate(self.qpus)
+        }
+        with Pool(processes=len(self.accelerators)) as pool:
+            results = []
+            for job in zip_longest(*jobs_per_qpu.values()):
+                result = pool.apply_async(_run_job, [self.accelerators, job])
+                results.append(result)
+            results = [result.get() for result in results]
+        results = [result for result in results if result is not None]
+        return results
+
     def run_experiments(self, experiments: list[Experiment]) -> list[Experiment]:
         """_summary_
 
@@ -78,3 +101,19 @@ def _run_func(accs: list[Accelerator], exp: Experiment) -> Experiment:
     except Exception as e:
         print(e)
     return exp
+
+
+def _run_job(
+    accs: list[Accelerator], jobs: tuple[CombinedJob | None]
+) -> CombinedJob | None:
+    pool_id = current_process()._identity[0] - 1  # TODO fix somehow
+    job = jobs[pool_id]
+    if job is None:
+        return None
+    job = job.job
+    try:
+        job.result_counts = accs[pool_id].run_and_get_counts(job.instance)
+
+    except Exception as e:
+        print(e)
+    return job
