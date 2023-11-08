1e062d2c6926d9d47208a99e4e7f236694e90fba
Philipp Seitz <philipp.seitz@tum.de>
2023-11-08 14:35:52 +0100

    Create initial test

--- src/provider/__init__.py
+++ src/provider/__init__.py
@@ -1,4 +1,5 @@
 """Module for runtime components."""
 from .accelerator import Accelerator, IBMQBackend
 from .accelerator_group import AcceleratorGroup
+from .generate_schedule import generate_simple_schedule
 from .scheduler import Scheduler
