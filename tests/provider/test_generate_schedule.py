6bf555aa28239896b921e5e2a270eadbae72523e
Philipp Seitz <philipp.seitz@tum.de>
2023-11-09 16:46:28 +0100

    Speed up test by simplifying problem

--- tests/provider/test_generate_schedule.py
+++ tests/provider/test_generate_schedule.py
@@ -3,14 +3,13 @@
 from src.circuits import create_ghz
 from src.common import IBMQBackend, job_from_circuit
 from src.provider import Accelerator, generate_simple_schedule
-from src.tools import optimize_circuit_offline
 
 
 def test_generate_simple_schedule() -> None:
     """_summary_"""
     accelerators = [
         Accelerator(IBMQBackend.BELEM, shot_time=1, reconfiguration_time=1),
-        Accelerator(IBMQBackend.QUITO, shot_time=2, reconfiguration_time=2),
+        Accelerator(IBMQBackend.QUITO, shot_time=1, reconfiguration_time=1),
     ]
     jobs = [job_from_circuit(create_ghz(i)) for i in range(2, 6)]
     schedule = generate_simple_schedule(jobs, accelerators, t_max=2**5)
