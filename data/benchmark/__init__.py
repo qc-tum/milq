5be1b574e1587676816225fd931f590bbc5781dd
Philipp Seitz <philipp.seitz@tum.de>
2023-11-13 16:46:55 +0100

    Make data + benchmark into modules for debugger

---
+++ data/benchmark/__init__.py
@@ -0,0 +1,8 @@
+from .generate_baseline_schedules import generate_baseline_schedule
+from .generate_milp_schedules import (
+    calculate_makespan,
+    generate_extended_schedule,
+    generate_simple_schedule,
+    set_up_base_lp,
+)
+from .benchmark import run_experiments
