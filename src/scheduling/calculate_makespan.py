78ed29f5ebaee1fe5f6a64afcf47f7e986bdb50c
Philipp Seitz <philipp.seitz@tum.de>
2023-12-14 13:05:26 +0100

    Simplify calculate makespan

--- src/scheduling/calculate_makespan.py
+++ src/scheduling/calculate_makespan.py
@@ -26,50 +26,13 @@ def calculate_makespan(
     Returns:
         float: The makespan of the schedule.
     """
-    s_times = pulp.makeDict(
-        [lp_instance.jobs, lp_instance.jobs, lp_instance.machines],
-        setup_times,
-        0,
-    )
-    p_times = pulp.makeDict(
-        [lp_instance.jobs[1:], lp_instance.machines],
+    return _calc_makespan(
+        jobs,
         process_times,
-        0,
+        setup_times,
+        lp_instance.jobs,
+        lp_instance.machines,
     )
-
-    assigned_machines: defaultdict[str, list[JobResultInfo]] = defaultdict(list)
-    for job in jobs:
-        assigned_machines[job.machine].append(job)
-    makespans = []
-    for machine, assigned_jobs in assigned_machines.items():
-        assigned_jobs_copy = deepcopy(assigned_jobs)
-        for job in sorted(assigned_jobs, key=lambda x: x.start_time):
-            # Find the last predecessor that is completed before the job starts
-            # this can technically change the correct predecessor to a wrong one
-            # because completion times are updated in the loop
-            # I'm not sure if copying before the loop corrects this
-            last_completed = _find_last_completed(job.name, assigned_jobs_copy, machine)
-
-            if job.start_time == 0.0:
-                last_completed = JobResultInfo("0", machine, 0.0, 0.0, 0)
-            job.start_time = next(
-                (
-                    j.completion_time
-                    for j in assigned_jobs
-                    if last_completed.name == j.name
-                ),
-                0.0,
-            )
-            # calculate p_j + s_ij
-            completion_time = (  # check if this order is correct
-                job.start_time
-                + p_times[job.name][machine]
-                + s_times[last_completed.name][job.name][machine]
-            )
-            job.completion_time = completion_time
-        makespans.append(max(job.completion_time for job in assigned_jobs))
-
-    return max(makespans)
 
 
 def _find_last_completed(
@@ -92,7 +55,6 @@ def _find_last_completed(
     return completed_before[0]
 
 
-# TODO Simplify?
 def calculate_bin_makespan(
     jobs: list[JobResultInfo],
     process_times: PTimes,
@@ -105,14 +67,25 @@ def calculate_bin_makespan(
     """
     lp_jobs = ["0"] + [job.name for job in jobs]  # TODO
     machines = list(accelerators.keys())
-    p_times = pulp.makeDict(
-        [lp_jobs[1:], machines],
-        process_times,
+    return _calc_makespan(jobs, process_times, setup_times, lp_jobs, machines, True)
+
+
+def _calc_makespan(
+    jobs: list[JobResultInfo],
+    process_times: PTimes,
+    setup_times: STimes,
+    job_names: list[str],
+    machines: list[str],
+    for_bin: bool = False,
+) -> float:
+    s_times = pulp.makeDict(
+        [job_names, job_names, machines],
+        setup_times,
         0,
     )
-    s_times = pulp.makeDict(
-        [lp_jobs, lp_jobs, machines],
-        setup_times,
+    p_times = pulp.makeDict(
+        [job_names[1:], machines],
+        process_times,
         0,
     )
 
@@ -121,24 +94,39 @@ def calculate_bin_makespan(
         assigned_machines[job.machine].append(job)
     makespans = []
     for machine, assigned_jobs in assigned_machines.items():
+        assigned_jobs_copy = deepcopy(assigned_jobs)
         for job in sorted(assigned_jobs, key=lambda x: x.start_time):
             # Find the last predecessor that is completed before the job starts
             # this can technically change the correct predecessor to a wrong one
             # because completion times are updated in the loop
             # I'm not sure if copying before the loop corrects this
-            last_completed = max(
-                (job for job in assigned_jobs), key=lambda x: x.completion_time
-            )
-            if job.start_time == 0.0:
-                last_completed = JobResultInfo("0", machine, 0.0, 0.0, 0)
-            job.start_time = last_completed.completion_time
+            if for_bin:
+                last_completed = max(
+                    (job for job in assigned_jobs), key=lambda x: x.completion_time
+                )
+                if job.start_time == 0.0:
+                    last_completed = JobResultInfo("0", machine, 0.0, 0.0, 0)
+                job.start_time = last_completed.completion_time
+            else:
+                last_completed = _find_last_completed(
+                    job.name, assigned_jobs_copy, machine
+                )
+                if job.start_time == 0.0:
+                    last_completed = JobResultInfo("0", machine, 0.0, 0.0, 0)
+                job.start_time = next(
+                    (
+                        j.completion_time
+                        for j in assigned_jobs
+                        if last_completed.name == j.name
+                    ),
+                    0.0,
+                )
             # calculate p_j + s_ij
-            completion_time = (  # check if this order is correct
+            job.completion_time = (  # check if this order is correct
                 last_completed.completion_time
                 + p_times[job.name][machine]
                 + s_times[last_completed.name][job.name][machine]
             )
-            job.completion_time = completion_time
         makespans.append(max(job.completion_time for job in assigned_jobs))
 
     return max(makespans)
