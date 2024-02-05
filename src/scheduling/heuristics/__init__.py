3f7ad366c58c5a0744dbc44817fdd40ecbaeabac
Philipp Seitz <philipp.seitz@tum.de>
2024-02-05 17:20:41 +0100

    Fix imports

--- src/scheduling/heuristics/__init__.py
+++ src/scheduling/heuristics/__init__.py
@@ -1,3 +1,6 @@
 """Heuristics for scheduling."""
-from .schedule import generate_heuristic_info_schedule, generate_heuristic_exec_schedule
+
+from .schedule import (
+    generate_heuristic_info_schedule,
+)  # , generate_heuristic_exec_schedule
 from .types import *
