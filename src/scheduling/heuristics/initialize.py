b095a7501971e84e28364c5fdcd1cdcac6de61d6
Philipp Seitz <philipp.seitz@tum.de>
2024-02-06 13:10:00 +0100

    Use circuit instead of job in initialize

--- src/scheduling/heuristics/initialize.py
+++ src/scheduling/heuristics/initialize.py
@@ -288,7 +288,8 @@ def _bin_schedule(
 
     for _bin in sorted(closed_bins, key=lambda x: x.index):
         machines[_bin.qpu].jobs += _bin.jobs
-        machines[_bin.qpu].buckets.append(Bucket(jobs=_bin.jobs))
+        # TODO: check if we need circuit or job
+        machines[_bin.qpu].buckets.append(Bucket(jobs=[job.circuit for job in  _bin.jobs]))
     return machines
 
 
