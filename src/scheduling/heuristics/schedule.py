9a9d05204d7be5ed4a8ef6164cdf42eed0a3ae25
Philipp Seitz <philipp.seitz@tum.de>
2024-02-06 15:50:44 +0100

    Add meta params to interface for now

--- src/scheduling/heuristics/schedule.py
+++ src/scheduling/heuristics/schedule.py
@@ -12,6 +12,8 @@ from .search import scatter_search
 def generate_heuristic_info_schedule(
     circuits: list[QuantumCircuit],
     accelerators: list[Accelerator],
+    num_iterations: int = 100,
+    num_elite_solutions: int = 10,
 ) -> tuple[list[ScheduledJob], float]:
     """Generates a schedule for the given jobs and accelerators using a scatter search heuristic.
 
@@ -21,16 +23,21 @@ def generate_heuristic_info_schedule(
     - Find meta-parameters for scatter search
     - Improve the heuristic (temperature, tabu list)
     - Find a good way to implement init options
-    
+
     Args:
         circuits (list[QuantumCircuit]): List of circuits (jobs) to schedule.
         accelerators (list[Accelerator]): List of accelerators to schedule on.
+        num_iterations (int, optional): Number of search iterations. Defaults to 100.
+        num_elite_solutions (int, optional): Max number of solutions to keep each round.
+            Defaults to 10.
 
     Returns:
         tuple[list[ScheduledJob], float]: The list of jobs with their assigned machine and
             the makespan of the schedule.
     """
-    schedule = scatter_search(circuits, accelerators)
+    schedule = scatter_search(
+        circuits, accelerators, num_iterations, num_elite_solutions
+    )
     combined_jobs = []
     for machine in schedule.machines:
 
