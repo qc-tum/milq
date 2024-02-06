0b13a663d66815195887cb2f58feac50cd53f314
Philipp Seitz <philipp.seitz@tum.de>
2024-02-06 16:17:02 +0100

    Update heursitic test

--- tests/scheduling/heuristics/test_schedule.py
+++ tests/scheduling/heuristics/test_schedule.py
@@ -1,11 +1,21 @@
 """_summary_"""
+
 from src.scheduling.heuristics import generate_heuristic_info_schedule
 
 from src.circuits import create_quantum_only_ghz
+from src.common import IBMQBackend
+from src.provider import Accelerator
 
 
 def test_generate_heuristic_info_schedule():
     """Test for generate_heuristic_info_schedule."""
-    circuits = [create_quantum_only_ghz(qubits) for qubits in range(2, 6)]
-    accelerators = {"a": 5, "b": 5}
-    assert len(generate_heuristic_info_schedule(circuits, accelerators)) > 0
+    circuits = [create_quantum_only_ghz(qubits) for qubits in range(8, 9)]
+    accelerators = [
+        Accelerator(IBMQBackend.BELEM, shot_time=1, reconfiguration_time=1),
+        Accelerator(IBMQBackend.NAIROBI, shot_time=1, reconfiguration_time=1),
+    ]
+    schedule, makespan = generate_heuristic_info_schedule(
+        circuits, accelerators, num_iterations=2, partition_size=3
+    )
+    assert 8 < len(schedule) < 12
+    assert 50 < makespan < 100
