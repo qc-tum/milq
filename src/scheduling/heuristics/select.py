8d5d423d515492751e9aa2babcc482cba03ef428
Philipp Seitz <philipp.seitz@tum.de>
2024-02-09 16:52:23 +0100

    Add diversification based on distance

--- src/scheduling/heuristics/select.py
+++ src/scheduling/heuristics/select.py
@@ -1,8 +1,10 @@
 """Evaluation and selection of solutions."""
 
+from uuid import UUID
+
 from src.provider import Accelerator
 
-from .types import Schedule, Bucket, MakespanInfo, is_feasible
+from .types import Schedule, Machine, Bucket, MakespanInfo, is_feasible
 
 
 def select_elite_solutions(
@@ -46,7 +48,36 @@ def select_best_solution(
     return population[0]
 
 
+def select_diverse_solutions(
+    population: list[Schedule], num_solutions: int
+) -> list[Schedule]:
+    """Selects the #num_solutions most diverse solutions from the population.
+
+    Args:
+        population (list[Schedule]): List of schedules to select from.
+        num_solutions (int): Number of solutions to select.
+
+    Returns:
+        list[Schedule]: The #num_solutions most diverse schedules.
+    """
+    return sorted(
+        population,
+        key=lambda x: sum(
+            _hamming_proxy(x, other) for other in population if other != x
+        ),
+    )[-num_solutions:]
+
+
 def evaluate_solution(schedule: Schedule, accelerators: list[Accelerator]) -> Schedule:
+    """Calculates and updates the makespan of a schedule.
+
+    Args:
+        schedule (Schedule): A schedule to evaluate.
+        accelerators (list[Accelerator]): The list of accelerators to schedule on.
+
+    Returns:
+        Schedule: The schedule with updated makespan and machine makespans.
+    """
     makespans = []
     for machine in schedule.machines:
         accelerator = next(acc for acc in accelerators if str(acc.uuid) == machine.id)
@@ -84,3 +115,29 @@ def _calc_machine_makespan(buckets: list[Bucket], accelerator: Accelerator) -> f
             + accelerator.compute_setup_time(last_completed.job, job.job)
         )
     return max(jobs, key=lambda j: j.completion_time).completion_time
+
+
+def _hamming_proxy(schedule: Schedule, other: Schedule) -> int:
+    """Hamming distance proxy function for schedules."""
+    num_buckets = max(len(schedule.machines), len(other.machines))
+    distance = 0
+    # should be same order
+    for machine1, machine2 in zip(schedule.machines, other.machines):
+        num_buckets = max(len(machine1.buckets), len(machine2.buckets))
+        jobs1 = _helper(machine1)
+        jobs2 = _helper(machine2)
+        for job in jobs1:
+            if job in jobs2:
+                distance += abs(jobs1[job] - jobs2[job])
+            else:
+                distance += num_buckets
+
+    return distance
+
+
+def _helper(machine: Machine) -> dict[UUID, int]:
+    return {
+        job.uuid: idx
+        for idx, bucket in enumerate(machine.buckets)
+        for job in bucket.jobs
+    }
