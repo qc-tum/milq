b0bd28f3a59d21d7c75cc2b994645b747c7db772
Philipp Seitz <philipp.seitz@tum.de>
2024-02-20 16:37:31 +0100

    Improve training loop

--- src/scheduling/learning/train.py
+++ src/scheduling/learning/train.py
@@ -1,17 +1,23 @@
-""""""
+"""Train a PPO model for the scheduling environment."""
 
 from typing import Any
+import logging
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
 
+
+from gymnasium.wrappers import FlattenObservation
 import gymnasium as gym
 
 
 def train_for_settings(settings: list[dict[str, Any]]):
     for i, setting in enumerate(settings):
+        logging.info("Training model for setting %d", i)
         env = gym.make("Scheduling-v0", **setting)
+        env = FlattenObservation(env)
         check_env(env)
+        logging.info("Environment checked successfully. Training model...")
         if i == 0:
             model = PPO(
                 "MlpPolicy", env, verbose=1
@@ -19,5 +25,6 @@ def train_for_settings(settings: list[dict[str, Any]]):
         else:
             model.set_env(env)
         model.learn(total_timesteps=10000)
+        logging.info("Setting completed.")
         env.close()
     model.save("ppo_scheduling")
