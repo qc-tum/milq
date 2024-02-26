c18bc7fc38c53dc9138e082f8d57571d770b292e
Philipp Seitz <philipp.seitz@tum.de>
2024-02-26 17:08:36 +0100

    Add qir conversion

---
+++ src/resource_estimation/running/convert.py
@@ -0,0 +1,17 @@
+"""Wrapper for the QIR conversion function using qiskit-qir."""
+
+from qiskit import QuantumCircuit
+from qiskit_qir import to_qir_module
+
+
+def convert_to_qir(circuit: QuantumCircuit) -> bytes:
+    """Convert a quantum circuit to QIR.
+
+    Args:
+        quantum_circuit (QuantumCircuit): The quantum circuit to convert.
+
+    Returns:
+        bytes: The QIR program as bytecode.
+    """
+    modlue, _ = to_qir_module(circuit)
+    return modlue.bitcode
