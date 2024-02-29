85e6d908d388e37290003fa72a65af2372e57372
Philipp Seitz <philipp.seitz@tum.de>
2024-02-29 11:50:07 +0100

    Rename module

--- src/scheduling/common/__init__.py
+++ src/scheduling/common/__init__.py
@@ -1,7 +1,7 @@
 """Modlue for shared scheduling functionality."""
 
 from .binpacking import do_bin_pack, do_bin_pack_proxy
-from .create_jobs import convert_circuits
+from .convert import convert_circuits
 from .evaluate import evaluate_solution
 from .fake_cut import cut_proxies
 from .types import *
