7c87409e201b98de400c42de46a06bbbbb61b689
Ectras <40306539+Ectras@users.noreply.github.com>
2024-03-20 14:40:35 +0100

    Fix broken import

--- src/provider/accelerator.py
+++ src/provider/accelerator.py
@@ -7,10 +7,9 @@ import logging
 from qiskit import QuantumCircuit, transpile
 from qiskit_aer import AerSimulator
 
-from src.circuits import generate_subcircuit
 from src.common import IBMQBackend, CombinedJob
 from src.resource_estimation import estimate_runtime, estimate_noise
-from src.tools import optimize_circuit_online
+from src.tools import optimize_circuit_online, generate_subcircuit
 
 
 class Accelerator:
