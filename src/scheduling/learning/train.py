49df98e8d7a08abb71e7406fce9ab193c976c8e4
Philipp Seitz <philipp.seitz@tum.de>
2024-03-26 09:29:58 +0100

    Improve train functions

--- src/scheduling/learning/train.py
+++ src/scheduling/learning/train.py
@@ -13,7 +13,21 @@ import gymnasium as gym
 from src.scheduling.common import Schedule
 
 
-def train_for_settings(settings: list[dict[str, Any]]):
+def train_for_settings(
+    settings: list[dict[str, Any]], total_timesteps: int = 100000
+) -> None:
+    """Train a PPO model for the scheduling environment.
+
+    The resulting model is saved as "ppo_scheduling".
+
+    Args:
+        settings (list[dict[str, Any]]):
+            The trainin settings consising of the trainng parameters:
+            - acclelerators: list[Accelerator | None]: The accelerators to use. None as padding.
+            - circuit: list[QuantumCircuit | User Circut]: The circuit to schedule.
+            And environmment configuration: max_stepts, penatly, and noise weight.
+        total_timesteps (int, optional): The number of timesteps for trainng. Defaults to 100000.
+    """
     for i, setting in enumerate(settings):
         logging.info("Training model for setting %d", i)
         env = gym.make("Scheduling-v0", **setting)
@@ -26,13 +40,24 @@ def train_for_settings(settings: list[dict[str, Any]]):
             )  # Create a single PPO model instance
         else:
             model.set_env(env)
-        model.learn(total_timesteps=1000000)
+        model.learn(total_timesteps)
         logging.info("Setting completed.")
         env.close()
     model.save("ppo_scheduling")
 
 
 def run_model(setting: dict[str, Any]) -> Schedule:
+    """Evaluate a trained PPO model for the scheduling environment.
+
+    Args:
+        setting (dict[str, Any]):
+            The evaluation setting consising of the trainng parameters:
+            - acclelerators: list[Accelerator | None]: The accelerators to use. None as padding.
+            - circuit: list[QuantumCircuit | User Circut]: The circuit to schedule.
+
+    Returns:
+        Schedule: The final schedule from the obesevation.
+    """
     env = gym.make("Scheduling-v0", **setting)
     env = FlattenObservation(env)
     logging.debug("loading rl model")
