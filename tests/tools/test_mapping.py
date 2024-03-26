158faa6adfe32538d9404d66501b61df2ef8450c
Ectras <40306539+Ectras@users.noreply.github.com>
2024-03-26 11:23:30 +0100

    Improve test for mapping
    Not super important, but was an open todo.

--- tests/tools/test_mapping.py
+++ tests/tools/test_mapping.py
@@ -1,5 +1,6 @@
 """"""
-from src.provider import Accelerator, IBMQBackend
+
+from src.provider import IBMQBackend
 from src.tools import map_circuit
 from tests.helpers import create_ghz
 
@@ -8,7 +9,19 @@ def test_map_circuit() -> None:
     """_summary_"""
     circuit = create_ghz(5)
     backend = IBMQBackend.BELEM
-    accelerator = Accelerator(backend)
-    circuit, _ = map_circuit(circuit, accelerator.backend)
-    # TODO better assertion
-    assert circuit.num_qubits == 5
+    circuit, _ = map_circuit(circuit, backend)
+
+    # Check the mapped circuit against the backend's coupling map
+    edges = backend.value().coupling_map.get_edges()
+    for [_, qubits, _] in circuit.data:
+        indices = [circuit.find_bit(qubit).index for qubit in qubits]
+        if len(indices) == 1:
+            # Single qubit gates are always possible
+            continue
+        elif len(indices) == 2:
+            # Check if the two qubits are indeed physically connected
+            [a, b] = indices
+            assert (a, b) in edges
+        else:
+            # Don't know how multi-qubit gates are handled in the coupling map
+            raise NotImplementedError
